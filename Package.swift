1ac6ca1dbce20c6af6ecebd322ab7d90c7881c49
Numan <muhammadnuman70@gmail.com>
2025-08-09 01:01:10 +0500

    fix: update Engine dependency to use local fixed version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "2.2.0"),
+        .package(url: "https://github.com/numandev1/Engine", branch: "fix/swift-concurrency"),
     ],
     targets: [
         .target(
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "2.1.15"),
+        .package(url: "https://github.com/numandev1/Engine", branch: "fix/swift-concurrency"),
     ],
     targets: [
         .target(
